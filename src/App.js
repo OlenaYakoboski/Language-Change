f703fa5fac3f0b92ebebfdd83220f4c3888bee70
olena yakoboski <misti1@ukr.net>
2024-01-08 14:08:47 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import { useTranslation } from 'react-i18next';
+import './App.css';
+
+function App() {
+
+  const{i18n,t}=useTranslation();
+
+const toggleLang=()=>{
+  i18n.changeLanguage(i18n.language ==='en' ? 'RU':'en')
+  
+}
+
+  return (
+    <div>
+      <h1>{t('Welcome to React')}</h1>
+      <button onClick={toggleLang}>{i18n.language==='en' ? 'RU':'EN'}
+      </button>
+    </div>
+  );
+}
+
+export default App;
